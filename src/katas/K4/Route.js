50290e6877d5a74453d7dc89b601fcd3e3ec7c47
bobbyquennell <bobbyquennell@gmail.com>
2017-07-17 15:51:06 +1000

    K4: create a navigation menu sparated with pipe

--- src/katas/K4/Route.js
+++ src/katas/K4/Route.js
@@ -7,12 +7,14 @@ import {
 
 import Home from './components/home/Home.js';
 import About from './components/about/About.js';
-
+import NavMenu from './components/common/NavMenu.js';
 const K4 = function(){
     return(
         <div>
             <Router>
                 <div>
+                    <NavMenu/>
+                    <hr/>
                     <Route exact path="/" component={Home}/>
                     <Route path="/about" component={About} />
                 </div>
