164b8b38cd6204e31135d1c56e358ee0dbfffea9
bobbyquennell <bobbyquennell@gmail.com>
2017-07-18 16:41:37 +1000

    K4:create an "add course" form in courseList page

--- src/katas/K4/components/course/CourseList.js
+++ src/katas/K4/components/course/CourseList.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const CourseList = function (){
-    return(
-        <div>
-            <h2>CourseList Page</h2>
-            <p>the courses will listed below</p>
-        </div>
-    );
-};
+class CourseList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            courseName: ""
+        };
+    }
+
+    submitHandler = (event)=>{
+        event.preventDefault();
+        alert(`adding course: ${this.state.courseName}`);
+    }
+    onChangeHandler = (event)=>{
+        this.setState({
+            courseName: event.target.value
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>CourseList Page</h2>
+                <form onSubmit={this.submitHandler}>
+                    <input type="text" placeholder="course name" value={this.state.courseName} onChange={this.onChangeHandler} required/>
+                    <button type="submit">Add Course</button>
+                </form>
+                <p>the courses will listed below</p>
+            </div>
+        );
+    }
+
+}
 export default CourseList;
