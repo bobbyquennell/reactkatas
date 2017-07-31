4e3cf53b16457a0740b409971c1ff6e562982328
bobbyquennell <bobbyquennell@gmail.com>
2017-07-31 23:17:20 +1000

    fix issue：<tr> cannot appear as a child of <table>

--- src/katas/K4/components/course/CourseTable.js
+++ src/katas/K4/components/course/CourseTable.js
@@ -4,6 +4,7 @@ import Style from './CourseTable-style';
 const CourseTable = (props)=>{
     return (
         <table style={Style.root}>
+            <tbody>{/*<tr> cannot appear as a child of <table> https://github.com/facebook/react/issues/5652 */}
             <tr>
                 <th style={Style.th}>Title</th>
                 <th style={Style.th}>Lecturer</th>
@@ -12,6 +13,7 @@ const CourseTable = (props)=>{
                 <th style={Style.th}>Time</th>
             </tr>
             { props.courses.map((course, index)=>{ return <CourseTableItem  key={index} {...course}/>; })}
+            </tbody>
         </table>
     );
 };
