ac8b592461cf0eb8e42fa7ee74eef1dd9402ada6
bobbyquennell <bobbyquennell@gmail.com>
2017-08-23 10:31:36 +1000

    input date/time types are not supported by firefox and safari.

--- src/katas/K4/components/common/DatePicker/DatePicker.js
+++ src/katas/K4/components/common/DatePicker/DatePicker.js
@@ -2,11 +2,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './DatePicker.scss';
-const DatePicker = function({ label, name, value, onChange }){
+const DatePicker = function({ label, name, value, placeHolder, onChange }){
   return (
     <div>
       <label className={styles.title} htmlFor={name}>{label}</label>
-      <input className={styles.root} name={name} value={value} type="date" onChange={onChange}/>
+      <input className={styles.root} name={name} value={value} type="text"
+        placeholder={placeHolder} onChange={onChange}/>
     </div>
 
   );
@@ -16,6 +17,7 @@ DatePicker.propTypes = {
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   value:PropTypes.string.isRequired,
+  placeHolder: PropTypes.string,
   onChange:PropTypes.func.isRequired
 };
 
--- src/katas/K4/components/common/TimePicker/TimePicker.js
+++ src/katas/K4/components/common/TimePicker/TimePicker.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import styles from './TimePicker.scss';
 import PropTypes from 'prop-types';
-const TimePicker = function({ label, name, value, onKeyPress }){
+const TimePicker = function({ label, name, value, placeHolder, onChange }){
   return (
     <div>
       <label className={styles.title} htmlFor={name}>{label}</label>
-      <input className={styles.root} name={name} type="time" value={value} onKeyPress={onKeyPress}/>
+      <input className={styles.root} name={name} type="text" value={value}
+      placeholder={placeHolder}
+       onChange={onChange}/>
     </div>
 
   );
@@ -16,7 +18,8 @@ TimePicker.propTypes = {
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
-  onKeyPress: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  placeHolder: PropTypes.string
 };
 
 export default TimePicker;
