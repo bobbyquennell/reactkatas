e91fe3ba599a63c885b7a9ec5e869a323c4a5e3d
bobbyquennell <bobbyquennell@gmail.com>
2017-09-08 17:10:39 +1000

    snapshot testing with Enzymen or React Test Utilities

--- src/katas/K4/components/course/CourseList.spec.js
+++ src/katas/K4/components/course/CourseList.spec.js
@@ -1,18 +1,27 @@
+import React from 'react';
 import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import React from 'react';
 import { CourseList } from './CourseList';
+import renderer from 'react-test-renderer';
 
-describe('CourseList', ()=>{
-  it('should render correctly', ()=>{
-    const props = {
-      courseList: [],
-      lecturers:[],
-      asyncInProgress: 0
-    };
-    const wrapper = shallow(
-      <CourseList {...props}/>
-    );
-    expect(toJson(wrapper)).toMatchSnapshot();
+describe('CourseList:', ()=>{
+  describe('When async operation is running', ()=>{
+    it('should render loading dots', ()=>{
+      const props = {
+        courseList: [],
+        lecturers:[],
+        asyncInProgress: 0
+      };
+      const wrapper = mount(
+        <CourseList {...props}/>
+      );
+      const wrapper2 = renderer.create(
+        <CourseList {...props}/>
+      ).toJSON();
+
+      expect(wrapper2).toMatchSnapshot();
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
   });
+
 });
